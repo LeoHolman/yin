7647492b8370ab3641b6414a76c1bf615c5b6bdb
LeoHolman <lah2719@g.rit.edu>
2018-11-04 10:47:18 -0500

    Convert to module pattern
    To increase maintainability and readability
    Begin JSON file for housing testing objects

---
+++ yin/js/displayFunctions.js
@@ -0,0 +1,36 @@
+'use strict';
+
+export function addSound (test,sourceId){
+    let target = document.getElementById(sourceId);
+    target.src = test.audio;
+}
+
+export function pickIncorrectOption(test){
+    let option;
+    option = test.options[Math.floor(Math.random() * test.options.length) ];
+    if (option == test.correctOption){
+        option = pickIncorrectOption(test);
+    } 
+    return option;
+}
+
+//evaluate user selection
+    //if correct highlight green
+    //repeat correct stimuli
+
+    //if incorrect highlight red
+    //repeat correct stimuli
+
+export function evaluateResponse(option,test) {
+    let response = document.getElementById(option).innerText;
+    if (response === test.correctOption){
+        changeBackgroundColor(option,"green");
+    } else {
+        changeBackgroundColor(option,"red");
+    }
+}
+    
+export function changeBackgroundColor(divId,color) {
+    let divIdHandle = document.getElementById(divId);
+    divIdHandle.style.backgroundColor = color;
+}
--- yin/js/distinguishing.js
+++ yin/js/distinguishing.js
@@ -1,25 +1,10 @@
+import * as df from './displayFunctions.js';
+
 //test objects
 var test0 = {
     audio : "../assets/sounds/test0.mp3",
     options : ["zhong1","zhong2","zhong3","zhong4"],
     get correctOption () {return this.options[0];} 
-}
-
-
-//display stimuli
-function addSound (test,sourceId){
-    let target = document.getElementById(sourceId);
-    target.src = test.audio;
-}
-
-//present options
-function pickIncorrectOption(test){
-    let option;
-    option = test.options[Math.floor(Math.random() * test.options.length) ];
-    if (option == test.correctOption){
-        option = pickIncorrectOption(test);
-    } 
-    return option;
 }
 
 var uniqueOptions = 0;
@@ -36,39 +21,13 @@ function presentOption(parentDiv,option){
     parentDivHandle.appendChild(newNode);
 }
 
-
-addSound(test0,"audioSource");
-presentOption("firstResponse",pickIncorrectOption(test0));
+//prompt user to select option
+df.addSound(test0,"audioSource");
+presentOption("firstResponse",df.pickIncorrectOption(test0));
 presentOption("secondResponse",test0.correctOption);
 
-
-//prompt user to select option
-
-
-//evaluate user selection
-    //if correct highlight green
-    //repeat correct stimuli
-
-    //if incorrect highlight red
-    //repeat correct stimuli
-
-function evaluateResponse(option,test) {
-    let response = document.getElementById(option).innerText;
-    if (response === test.correctOption){
-        changeBackgroundColor(option,"green");
-    } else {
-        changeBackgroundColor(option,"red");
-    }
-}
-
-function changeBackgroundColor(divId,color) {
-    let divIdHandle = document.getElementById(divId);
-    divIdHandle.style.backgroundColor = color;
-}
-
-
-evaluateResponse("option0",test0);
-evaluateResponse("option1",test0);
 //set evaluation to only occur onclick
+document.getElementById("option0").addEventListener("click",function(){df.evaluateResponse("option0",test0);});
 
+document.getElementById("option1").addEventListener("click",function(){df.evaluateResponse("option1",test0);});
 
