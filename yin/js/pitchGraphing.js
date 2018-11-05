8d95da7a10a044d43ffb13501491283696ee016a
LeoHolman <lah2719@g.rit.edu>
2018-11-05 15:44:31 -0500

    Add predefined pitch graph drawing function

--- yin/js/pitchGraphing.js
+++ yin/js/pitchGraphing.js
@@ -1,15 +1,20 @@
 // d3 = import("https://d3js.org/d3.v5.js"); 
 //why can i use d3 without importing it?
 
-let div = document.getElementById("visualization");
-let data1 = d3.csv("../praat/zhong_pitchtier.csv",function(data){console.log(data);})
-d3.select(div).append("p").text("d3 is working");
+import * as drawf from './drawingFunctions.js'
+
+
+
 
 
 //import data
+// let data1 = d3.csv("../praat/zhong_pitchtier.csv",function(data){console.log(data);})
 
 //select div target
-//draw canvas
-//draw guidelines
+//draw canvas with guidelines
+var width = 1000;
+var height = 400;
+
+drawf.drawPitchChart('#visualization',width,height);
 //draw points
 //draw lines between points
