9f07b0ab8a0eb47239e9fc521d8db26e45aa3ead
LeoHolman <lah2719@g.rit.edu>
2018-11-06 20:38:55 -0500

    Add function to read tsv
    Hard-coded for a single example
    example is upside-down

--- yin/js/pitchGraphing.js
+++ yin/js/pitchGraphing.js
@@ -1,14 +1,10 @@
 // d3 = import("https://d3js.org/d3.v5.js"); 
 //why can i use d3 without importing it?
-
 import * as drawf from './drawingFunctions.js'
 
 
-
-
-
 //import data
-// let data1 = d3.csv("../praat/zhong_pitchtier.csv",function(data){console.log(data);})
+// let data1 = d3.tsv("../praat/zhong_pitchtier.csv",function(data){console.log(data);})
 
 //select div target
 //draw canvas with guidelines
@@ -17,4 +13,14 @@ var height = 400;
 
 drawf.drawPitchChart('#visualization',width,height);
 //draw points
-//draw lines between points
+
+d3.tsv("../praat/zhong_pitchtier.csv",function(data) {
+    console.log(data.frequency);
+
+    d3.select("#visualization svg")
+        .append("circle")
+        .attr("cx",data.time*500)
+        .attr("cy",data.frequency/3)
+        .attr("r",5);
+
+});
