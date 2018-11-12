5557c69fe45252211600d3730bdc1232789884a2
LeoHolman <lah2719@g.rit.edu>
2018-11-12 11:41:20 -0500

    Pitch points display right-side up
    y = height - freqency instead of y = frequency

--- yin/js/pitchGraphing.js
+++ yin/js/pitchGraphing.js
@@ -1,26 +1,25 @@
 // d3 = import("https://d3js.org/d3.v5.js"); 
-//why can i use d3 without importing it?
+//how can i use d3 without importing it?
 import * as drawf from './drawingFunctions.js'
 
 
-//import data
-// let data1 = d3.tsv("../praat/zhong_pitchtier.csv",function(data){console.log(data);})
-
-//select div target
 //draw canvas with guidelines
 var width = 1000;
-var height = 400;
+var height = 350;
 
+//select div target
+//import data
 drawf.drawPitchChart('#visualization',width,height);
-//draw points
 
+//draw points
 d3.tsv("../praat/zhong_pitchtier.csv",function(data) {
     console.log(data.frequency);
 
     d3.select("#visualization svg")
         .append("circle")
-        .attr("cx",data.time*500)
-        .attr("cy",data.frequency/3)
-        .attr("r",5);
+        .attr("cx",data.time*(width/2))
+        .attr("cy",height - data.frequency)
+        .attr("r",5)
+        .style("fill","red");
 
 });
