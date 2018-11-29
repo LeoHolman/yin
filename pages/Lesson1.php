3a119d4bacbdca800f7b31a7f4ba3f5938586961
cmccullough012 <cxm1880@rit.edu>
2018-11-28 19:49:19 -0500

    Small change to activity one styling

--- pages/Lesson1.php
+++ pages/Lesson1.php
@@ -73,11 +73,11 @@ include "../head.php";
         </div>
         <div id="secondResponse" class="response"></div>
         <div id = "feedback-box">
-            <div id ="correct" class = "hide correct">
+            <div id ="correct" class = "hide correctfeed">
                 <h3>Correct!</h3>
                 <p>Further description here.</p>
             </div>
-            <div id ="incorrect" class = "hide incorrect">
+            <div id ="incorrect" class = "hide incorrectfeed">
                 <h3>Incorrect...</h3>
                 <p>Further description here.</p>
             </div>
