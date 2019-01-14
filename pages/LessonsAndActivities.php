fcd4cf47a7290f6f982c5dc85b3a7e9aa282b3db
cmccullough012 <cxm1880@rit.edu>
2019-01-14 10:53:44 -0500

    Lessons and Activities hover state improved
    Restructured with div container to hold head and description

--- pages/LessonsAndActivities.php
+++ pages/LessonsAndActivities.php
@@ -8,36 +8,63 @@ include "../head.php";
 
 <div class = "main honeycomb">
     <div id = "one" class = "comb">
+        <div class = "invisible">
+            <h1 class = "head">One</h1>
+            <p class = "desc">Lesson: Lexical tones<br>Activity: Tone distinguishing</p>
+        </div>
+        
+
+
+<!--
         <div class = "L-head">
         <h2>Lesson One</h2>
         </div>
         <div class = "A-head">
         <h2>Activity One</h2>
         </div>
+-->
     </div>
     <div id = "two" class = "comb">
+        <div class = "invisible">
+            <h1 class = "head">Two</h1>
+            <p class = "desc">Lesson: Tone characteristics<br>Activity: Tone identification</p>
+        </div>
+<!--
         <div class = "L-head">
         <h2 >Lesson Two</h2>
         </div>
         <div class = "A-head">    
         <h2>Activity Two</h2>
         </div>
+-->
     </div>
     <div id = "three" class = "comb">
+        <div class = "invisible">
+            <h1 class = "head">Three</h1>
+            <p class = "desc">Lesson: Relative tones<br>Activity: Tone mimicking</p>
+        </div>
+<!--
         <div class = "L-head">
         <h2>Lesson Three</h2>
         </div>
         <div class = "A-head">
         <h2>Activity Three</h2>
         </div>
+-->
     </div>
     <div id = "four" class = "comb">
+        <div class = "invisible">
+            <h1 class = "head">Four</h1>
+            <p class = "desc">Lesson: Tone initials and finals<br>Activity: Tone production</p>
+        </div>
+<!--
         <div class = "L-head">
         <h2>Lesson Four</h2>
         </div>
         <div class = "A-head">
         <h2>Activity Four</h2>
         </div>
+-->
     </div>
 
 
