2a741d90b4104a21cd52a46e00a3b84603dadf72
cmccullough012 <cxm1880@rit.edu>
2019-01-15 13:19:45 -0500

    Fixed JS
    Switched from calling ids to class names so the same code can be used for every activity page

---
+++ pages/LessonTemplate.php
@@ -0,0 +1,58 @@
+<?php
+$title = "Yin - Lesson One";
+$style = "../css/style.css";
+$style2 = "../css/LAstyle.css";
+$prefix = "../";
+include "../head.php";
+?>
+
+<div id = "main-wrap">
+<div class = "main container-one LA">
+    <section id = "lesson-one" class = "lesson">
+        <h1>Lesson #</h1><hr>
+        <h2>Topic</h2>
+    </section>
+    <section id = "lesson-#-ref" class = "hide view-2 lesson">
+        <div id = "ref-title"><h1>Lesson #</h1><hr>
+            <h2>Reference: Topic</h2>
+        </div>
+        <button type = "button" id = "btn-to-lesson" onclick = "closeActivity()">Return to Lesson</button>
+        
+    </section>
+    <section id = "activity-one-prompt" class = "activity">
+        <h1>Activity #</h1><hr>
+        <h2>Title</h2>
+        <p>Instructions</p>
+        <p>Are you ready to test what you learned with with this activity?</p>
+        <button type = "button" onclick = "openActivity();" id = "begin-btn">Begin</button>
+    
+    </section>
+    <section id = "activity-#" class = "hide view-2 activity">
+        <div id = "ref-title"><h1>Activity #</h1><hr>
+            <h2>Title</h2></div>
+         [activity components here]
+        <div id = "feedback-box">
+            <div id ="correct" class = "hide correctfeed">
+                <h3>Correct!</h3>
+                <p>Further description here.</p>
+            </div>
+            <div id ="incorrect" class = "hide incorrectfeed">
+                <h3>Incorrect...</h3>
+                <p>Further description here.</p>
+            </div>
+        </div>
+        <button type = "button" id = "continue-btn">Continue</button>
+    </section>
+
+</div>
+</div>
+
+<div>
+<p>footer</p>
+</div>
+
+    <script type="module" src="../js/distinguishing.js"></script>
+    <script type="module" src="../js/displayFunctions.js"></script>
+
+</body>
+
