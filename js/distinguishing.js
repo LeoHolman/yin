3243633bc619946516099490f9fa49b58499b70b
Leo Holman (RIT Student) <lah2719@ad.rit.edu>
2019-01-16 12:13:35 -0500

    Load tests from external JSON file

--- js/distinguishing.js
+++ js/distinguishing.js
@@ -1,31 +1,19 @@
 import * as df from './displayFunctions.js';
 
 var tests;
+var testsParsed;
 //Access tests.json
 $.getJSON("../js/tests.json", function(json){
-    console.log(json);
     tests = json;
-    console.log(tests);
+
+    //add stimuli
+    df.addSound(tests.test0,"audioSource");
+
+    //present user with options
+    df.presentOption("firstResponse",df.pickIncorrectOption(tests.test0));
+    df.presentOption("secondResponse",tests.test0.correctOption);
+
+    //set evaluation to occur onclick
+    df.addEvaluator("option0",tests.test0);
+    df.addEvaluator("option1",tests.test0);
 });
-
-// console.log(tests);
-//test objects
-// var test0 = {
-//     audio : "../assets/sounds/test0.mp3",
-//     options : ["zhong1","zhong2","zhong3","zhong4"],
-//     get correctOption () {return this.options[0];} 
-// }
-
-//add stimuli
-df.addSound(test0,"audioSource");
-
-//present user with options
-df.presentOption("firstResponse",df.pickIncorrectOption(test0));
-df.presentOption("secondResponse",test0.correctOption);
-
-//set evaluation to occur onclick
-df.addEvaluator("option0",test0);
-df.addEvaluator("option1",test0);
-
-
-
