d2fe5b2f224b10d59c6e1efeaa4fc0f90139780f
Leo Holman (RIT Student) <lah2719@ad.rit.edu>
2019-01-17 12:53:27 -0500

    Group test functions together in distinguishing

--- js/distinguishing.js
+++ js/distinguishing.js
@@ -6,14 +6,28 @@ var testsParsed;
 $.getJSON("../js/tests.json", function(json){
     tests = json;
 
+    // //add stimuli
+    // df.addSound(tests.test0,"audioSource");
+
+    // //present user with options
+    // df.presentOption("firstResponse",df.pickIncorrectOption(tests.test0));
+    // df.presentOption("secondResponse",tests.test0.correctOption);
+
+    // //set evaluation to occur onclick
+    // df.addEvaluator("option0",tests.test0);
+    // df.addEvaluator("option1",tests.test0);
+    newTest(tests.test8);
+});
+
+function newTest(testNumber){
     //add stimuli
-    df.addSound(tests.test0,"audioSource");
+    df.addSound(testNumber, "audioSource");
 
     //present user with options
-    df.presentOption("firstResponse",df.pickIncorrectOption(tests.test0));
-    df.presentOption("secondResponse",tests.test0.correctOption);
+    df.presentOption("firstResponse",df.pickIncorrectOption(testNumber));
+    df.presentOption("secondResponse",testNumber.correctOption);
 
     //set evaluation to occur onclick
-    df.addEvaluator("option0",tests.test0);
-    df.addEvaluator("option1",tests.test0);
-});
+    df.addEvaluator("option0",testNumber);
+    df.addEvaluator("option1",testNumber);
+}
