c0401609f0d32f21d1ed71596d8e0cc5dbebdf79
Leo Holman <lah2719@rit.edu>
2019-02-15 11:54:13 -0500

    Update drawPitchChart to show pitch without baseline

--- js/drawingFunctions.js
+++ js/drawingFunctions.js
@@ -48,6 +48,9 @@ export function drawPitchChart(divID, width, height) {
 export function drawPitchCurve(dataset, width, height, baseline=(height/2), standardDeviation=0) {
     d3.tsv(dataset, function(data) {
 	let zScore = stats.calcZScore(baseline,data.frequency,standardDeviation);    
+	if(isNaN(zScore)){
+		zScore = 0;
+	}
         d3.select("#visualization svg")
             .append("circle")
             .attr("cx", data.time * (width / 2))
