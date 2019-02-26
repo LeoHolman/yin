04144bac53b2289aa9bee096ee7d4c1f93818e09
Leo Holman <lah2719@g.rit.edu>
2019-02-26 11:33:11 -0500

    Set Lesson1 and Lesson1 to use pi.LoadTests()

--- js/distinguishing.js
+++ js/distinguishing.js
@@ -1,66 +1,6 @@
 import * as df from './displayFunctions.js';
 import * as pi from './pageInteractions.js';
 
-//var tests;
-//var testsArray = [];
-//var shownTests = [];
-//
-////Access tests.json
-//$.getJSON("../js/tests.json", function(json){
-//    tests = json;
-//
-//    for(var i in tests){
-//        testsArray.push(tests [i]);
-//    }
-//
-//    //Initial test
-//    newTest();
-//});
-//
-//function newTest(){
-//    document.getElementById("correct").classList.add("hide");
-//    document.getElementById("incorrect").classList.add("hide");
-//    
-//    //find new test
-//    var thisTestNumber = df.getRandomInt(12);
-//    while (shownTests.includes(thisTestNumber)){
-//        thisTestNumber = df.getRandomInt(12);
-//    }
-//    shownTests.push(thisTestNumber);
-//    var thisTest = testsArray[thisTestNumber];
-//    
-//    //add stimuli
-//    df.addSound(thisTest, "audioSource");
-//
-//    //present user with options
-//    if (Math.random() > 0.5){
-//        df.presentOption("firstResponse",df.pickIncorrectOption(thisTest));
-//        df.presentOption("secondResponse",thisTest.correctOption);
-//    } else {
-//        df.presentOption("firstResponse",thisTest.correctOption);
-//        df.presentOption("secondResponse",df.pickIncorrectOption(thisTest));
-//    }
-//
-//    //set evaluation to occur onclick
-//    df.addEvaluator(document.getElementById("firstResponse").firstChild.id,thisTest);
-//    df.addEvaluator(document.getElementById("secondResponse").firstChild.id,thisTest);
-//    
-//    //reset responseGiven for new test
-//    df.setResponseGiven(false);
-//
-//    if (shownTests.length == 10){
-//        document.getElementById("continueButton").removeEventListener("click", function(){
-//            newTest();
-//        });
-//        
-//        document.getElementById("continueButton").innerHTML="Finish";
-//        
-//        document.getElementById("continueButton").addEventListener("click", function(){
-//            df.showScoreCard("activity-one", "lesson-one-ref");
-//        })
-//    }
-//}
-
 pi.loadTests(1);
 
 //create new test on click
--- js/identifying.js
+++ js/identifying.js
@@ -1,69 +1,11 @@
 import * as df from './displayFunctions.js';
 import * as pi from './pageInteractions.js';
 
-var tests;
-var testsArray = [];
-var shownTests = [];
-
-//Access tests.json
-$.getJSON("../js/tests.json", function(json){
-    tests = json;
-
-    for(var i in tests){
-        testsArray.push(tests [i]);
-    }
-
-    //Initial test
-    newTest();
-});
-
-function newTest(){
-    df.setResponseGiven(false);
-    document.getElementById("correct").classList.add("hide");
-    document.getElementById("incorrect").classList.add("hide");
-    
-    df.clearResultStyle();
-    //find new test
-    var thisTestNumber = df.getRandomInt(12);
-    while (shownTests.includes(thisTestNumber)){
-        thisTestNumber = df.getRandomInt(12);
-    }
-    shownTests.push(thisTestNumber);
-    var thisTest = testsArray[thisTestNumber];
-    
-    //add stimuli
-    console.log(thisTest);
-    df.addSound(thisTest, "audioSource");
-
-    //present user with options
-    df.presentOption("firstResponse",thisTest.options[0]);
-    df.presentOption("secondResponse",thisTest.options[1]);
-    df.presentOption("thirdResponse",thisTest.options[2]);
-    df.presentOption("fourthResponse",thisTest.options[3]);
-
-    //set evaluation to occur onclick
-    df.addEvaluator(document.getElementById("firstResponse").firstChild.id,thisTest);
-    df.addEvaluator(document.getElementById("secondResponse").firstChild.id,thisTest);
-    df.addEvaluator(document.getElementById("thirdResponse").firstChild.id,thisTest);
-    df.addEvaluator(document.getElementById("fourthResponse").firstChild.id,thisTest);
-    
-   if (shownTests.length == 10){
-        document.getElementById("continueButton").removeEventListener("click", function(){
-            newTest();
-        });
-        
-        document.getElementById("continueButton").innerHTML="Finish";
-        
-        document.getElementById("continueButton").addEventListener("click", function(){
-            df.showScoreCard("activity-two","lesson-two-ref");
-        })
-    }
-    
-}
+pi.loadTests(2);
 
 //create new test on click
 document.getElementById("continueButton").addEventListener("click", function(){
-   pi.newTest();
+   pi.newTest(2);
     
 });
 
