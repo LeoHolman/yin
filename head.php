18567123f19cfd68b2dc0dbf049ea2220a69419a
cmccullough012 <cxm1880@rit.edu>
2019-04-13 10:29:31 -0400

    small changes

--- head.php
+++ head.php
@@ -18,6 +18,6 @@
         <ul>
             <li><a href = "<?php echo $prefix ?>pages/About.php" class ="<?php echo $aboutClass ?>">About</a></li>
             <li><a href = "<?php echo $prefix ?>pages/LessonsAndActivities.php" class ="<?php echo $LAClass ?>">Lessons and Activities</a></li>
-            <li><a href = "<?php echo $prefix ?>index.php" class ="<?php echo $indexClass ?>"> Home</a></li>
+<!--            <li><a href = "<?php echo $prefix ?>index.php" class ="<?php echo $indexClass ?>"> Home</a></li>-->
         </ul>
       </nav>
